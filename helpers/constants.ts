33fbfaf2ca7cf7f5206f3aa40f401f8f03a108fa
icedTet <ic3dplasma@gmail.com>
2022-07-04 02:18:12 -0700

    changed domain

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -1,2 +1,2 @@
-// export const API_DOMAIN = 'https://api.gunnelimination.com';
-export const API_DOMAIN = 'http://localhost:443';
+export const API_DOMAIN = 'https://apimathcamp.tet.moe:883';
+// export const API_DOMAIN = 'http://localhost:443';
