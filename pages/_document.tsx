8cb7fcfbf134a3bb5ac50b5d6ab40b05e7d83895
icedTet <ic3dplasma@gmail.com>
2022-07-04 02:43:39 -0700

    favicon change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class GDocument extends Document {
             <Html className="h-full dark">
                 <Head>
                     <meta charSet="utf-8" />
-                    <link rel="icon" href="/favicon.png" />
+                    <link rel="icon" href="/favicon.ico" />
                 </Head>
                 <body className="h-full dark:bg-gradient-to-br dark:from-zinc-900 dark:to-black dark:text-white">
                     <Main />
